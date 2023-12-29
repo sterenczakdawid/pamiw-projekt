ad75a664aadcaa62395c1343e61d46678e5762d2
Dawid <sterenczak.dawid@gmail.com>
2023-12-29 14:30:24 +0100

    core

---
+++ src/app/core/interfaces/page.interface.ts
@@ -0,0 +1,28 @@
+export interface Page<T> {
+  content: T[];
+  pageable: {
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    pageSize: number;
+    pageNumber: number;
+    unpaged: boolean;
+    paged: boolean;
+  };
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
